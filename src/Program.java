138d043839eb1716da45f96c20006044e17d01e5
Brian Grin <bgrin@miyoki.ch>
2020-11-16 15:54:05 +0100

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,10 @@
+public class Program {
+
+	public static void main(String[] args) {
+		Salle2Cours s2c = null;
+		
+		s2c.display();
+
+	}
+
+}
---
+++ src/Salle2Cours.java
@@ -0,0 +1,10 @@
+public class Salle2Cours {
+	
+	public Salle2Cours() {
+		
+	}
+	
+	public void display() {
+		
+	}
+}
